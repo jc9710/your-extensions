dd89d089d6e089330eb205167ac131f754dc34d2
James Cockbain <jcockbain97@gmail.com>
2022-05-18 08:53:42 +0100

    Implement feature layer filter

--- widgets/vehicle-parameters/src/runtime/widget.tsx
+++ widgets/vehicle-parameters/src/runtime/widget.tsx
@@ -7,45 +7,14 @@ import { TextInput, Label } from 'jimu-ui'
 
 export default class Widget extends React.PureComponent<AllWidgetProps<any>, any> {
   state = {
-    jimuMapView: null,
-    latitude: "",
-    longitude: "",
     height: "",
     length: "",
     width: "",
     weight: ""
   };
 
-  activeViewChangeHandler = (jmv: JimuMapView) => {
-    if (jmv) {
-      this.setState({
-        jimuMapView: jmv
-      });
-
-      jmv.view.on("pointer-move", (evt) => {
-        const point: Point = this.state.jimuMapView.view.toMap({
-          x: evt.x,
-          y: evt.y
-        });
-        this.setState({
-          latitude: point.latitude.toFixed(3),
-          longitude: point.longitude.toFixed(3)
-        });
-      });
-    }
-  };
-
   render() {
     return <div className="widget-starter jimu-widget">
-      {this.props.hasOwnProperty("useMapWidgetIds") &&
-         this.props.useMapWidgetIds &&
-         this.props.useMapWidgetIds[0] && (
-           <JimuMapViewComponent
-             useMapWidgetId={this.props.useMapWidgetIds?.[0]}
-             onActiveViewChange={this.activeViewChangeHandler}
-           />
-         )
-       }
       <label>
           Vehicle Height:{' '}
           <TextInput
